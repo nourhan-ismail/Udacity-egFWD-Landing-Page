722fc4a6c8d06eddc0733e5e4084635b60178167
nourhan-ismail <73067280+nourhan-ismail@users.noreply.github.com>
2021-08-24 12:12:02 +0200

    Improved navigation bar performance

--- js/app.js
+++ js/app.js
@@ -18,69 +18,86 @@
  * 
 */
 
-
 //returns nodelist which contains all sections
 const sections = document.querySelectorAll("section");
 
 //the navBar for populating it with links for each section
 const navBar = document.getElementById("navbar__list");
 
+//to append dynmically the navigation links when created
+let docFragment = document.createDocumentFragment();
 /**
  * End Global Variables
  * 
 */
 
+
 /**
  Start Main functions
  */
+//build the nav
+function buildNav(){
+    let sectionsLength = sections.length;
+    for (let i = 0; i < sectionsLength; i++) {
+        let newNavItem = document.createElement("li");
+        newNavItem.innerHTML = sections[i].getAttribute("data-nav");
+        newNavItem.classList.add("menu__link");
+        docFragment.appendChild(newNavItem);
+    
+        // Scroll to section on link click
+        newNavItem.addEventListener("click", function () {
+            sections[i].scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        });
+  
+    }
+    navBar.appendChild(docFragment);
+    navBar.addEventListener("click",setActiveLink);
+ 
+}
+
+//These functions are called when scroll event is trigerred
 const setActiveClass = () => {
     sections.forEach((inactiveSection) => {
-        if (inactiveSection.classList.contains("your-active-class")){
+        if (inactiveSection.classList.contains("your-active-class")) {
             inactiveSection.classList.remove("your-active-class");
         }
-            
-        });
+
+    });
     sections.forEach((section) => {
         let currentSectionTop = section.getBoundingClientRect().top;
-      
+
         //0 value to enure that the top is not a negative number, so sections that are not in the viewport are ignored
-        //the value 415 is an arbitrary value to ensure that the current section is the active one
-        //since there is no two sections have top less than or equal to 450, so top 450 will only correspond to one section (which is the active section in the viewport)
-        if (currentSectionTop >= 0 && currentSectionTop <= 400) {
+        //the value 410 is an arbitrary value to ensure that the current section is the active one
+        //since there is no two sections have top less than or equal to 410, so top 450 will only correspond to one section (which is the active section in the viewport)
+        if (currentSectionTop >= 0 && currentSectionTop <= 410) {
             section.classList.add("your-active-class");
         }
     });
-  
+
 }
 
 
+//to change the styling of the clicked link
+function setActiveLink(e){
+    const navItems = document.getElementsByTagName('li');
 
-// build the nav
-sections.forEach((section, index, array) => {
-    //create li element
-    let newNavItem = document.createElement("li");
-    newNavItem.innerHTML = section.getAttribute("data-nav");
-    newNavItem.classList.add("menu__link");
+    for(let i = 0;i<navItems.length;i++)
+   {
+        navItems[i].style.background = "white";
+       navItems[i].style.color = "black";
+    }
+    e.target.style.background = "black";
+   e.target.style.color = "white";
+    
+}
 
-    navBar.appendChild(newNavItem);
 
-    // Scroll to section on link click
-    newNavItem.addEventListener("click", function () {
-        section.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-        //styling for the active link
-
-        navBar.childNodes.forEach(element => {
-            element.classList.remove("active__link");
-            element.style.color = "black";
-        });
-        newNavItem.classList.add("active__link");
-        newNavItem.style.color = "white";
-    });
-
-});
-
+//to ensure that the DOM is ready
+document.addEventListener('DOMContentLoaded', buildNav);
 
 //set the section active state
 document.addEventListener("scroll", setActiveClass);
+
+
+
 
